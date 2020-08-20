d6368a72668a069482532afd3fea3ab52538e935
Tiago Ferreira <tiagoandreaf95@gmail.com>
2020-08-20 18:10:43 +0100

    added routing mechanisms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from "src/shared/dropdown.directive";
 import { RecipeService } from "./recipes/recipe.service";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
+import { AppRoutingModule } from "./app-routing.module";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 @NgModule({
   declarations: [
@@ -24,8 +27,10 @@ import { ShoppingListService } from "./shopping-list/shopping-list.service";
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent,
   ],
-  imports: [FormsModule, BrowserModule],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent],
 })
